351a270ff4c04f968d617aa90fa33dba017086a8
Calvenn <calvenkh@gmail.com>
2025-08-21 00:50:41 +0800

    for merge purpose

---
+++ src/Control/PharmacyReport.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Control;
+
+import Entity.MedRecord;
+import Entity.Patient;
+import Entity.Medicine;
+import adt.List;
+import adt.LinkedHashMap;
+import java.util.Scanner;
+
+public class PharmacyReport {
+}
